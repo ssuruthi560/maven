b3012f4d1aa353f130e19eee4833f47391effa72
S_Suruthi <ssuruthi560@gmail.com>
2025-02-28 10:47:38 +0530

    Add files via upload

---
+++ MindsphereApplication.java
@@ -0,0 +1,13 @@
+package com.mindsphere._4.mindsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindsphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindsphereApplication.class, args);
+	}
+
+}
---
+++ MindsphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindsphere._4.mindsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindsphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
